2ef59f6ea49766c16975e1a3a0bbb0d9a008c629
VasilyevV <109423236+VasilyevV@users.noreply.github.com>
2022-10-06 09:30:30 +0400

    Add files via upload

---
+++ DataBank.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using Microsoft.Data.Sqlite;
+
+namespace DEADBEEF
+{
+    internal class DataBank
+    {
+        internal void Set(string FileName, string md5Hash)
+        {
+            var connection = new SqliteConnection("Data Source=Databank.db");
+            connection.Open();
+
+            var command = new SqliteCommand();
+            command.Connection = connection;
+            command.CommandText = @"INSERT INTO Hashs (Hash, FileName) VALUES (@Hash, @FileName)";
+            command.Parameters.Add(new SqliteParameter("@Hash", md5Hash));
+            command.Parameters.Add(new SqliteParameter("@FileName", FileName));
+
+            command.ExecuteNonQuery();
+
+            Console.WriteLine("Hash добавлен");
+        }
+
+        internal bool Get (string md5Hash)
+        {
+            var HashList = new List<string>();
+            int count = 0;
+            
+            string sqlExpression = "SELECT * FROM Hashs";
+            var connection = new SqliteConnection("Data Source=Databank.db");
+            connection.Open();
+            var command = new SqliteCommand(sqlExpression, connection);
+            SqliteDataReader reader = command.ExecuteReader();
+
+            if (reader.HasRows) // если есть данные
+            {
+                while (reader.Read())
+                {
+                    var hash = reader["Hash"].ToString();
+                    HashList.Add(hash);
+                }
+                
+                foreach(var str in HashList)
+                    if(str == md5Hash)
+                        count++;
+
+                if(count>0)        
+                    return true;
+                return false;
+            }
+            else
+                return false;
+        }
+    }
+}
---
+++ GetHash.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace DEADBEEF
+{
+    internal static class GetHash
+    {
+        internal static string Hash(string path)
+        {
+            var md5 = MD5.Create();
+            var stream = File.OpenRead(path);
+            string result = BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", String.Empty);
+            return result;
+        }
+    }
+}
